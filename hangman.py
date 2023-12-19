a6c6c07da94e98fdce8840d13b3edc4f4de18813
Siim Muutra <smuutra@gmail.com>
2023-12-20 01:26:25 +0200

    Hangmani kood

--- hangman.py
+++ hangman.py
@@ -1,34 +1,64 @@
-from time import sleep
-from random import randint
+import random
 
-def vali_sõna(arv):
-    with open('sõnad.txt', encoding='utf-8') as f:
-        sisu = f.read()
-        sisu = sisu.split('\n')
-        if arv == 'r' or arv == 'R':
-            sisu = sisu[0].split(',')
-            return sisu[randint(0,6)]
-        if arv == '8':
-            sisu = sisu[1].split(',')
-            return sisu[randint(0,22)]
-        if arv == '9':
-            sisu = sisu[2].split(',')
-            return sisu[randint(0,6)]
-        if arv == '10':
-            sisu = sisu[3].split(',')
-            return sisu[randint(0,9)]
-        if arv == '11':
-            sisu = sisu[4].split(',')
-            return sisu[randint(0,7)]
+def poomismäng(sõna):
+    äraarvatud_tähed = set()
+    elud = 6
+    lüngad = ['_' if täht.isalpha() else ' ' for täht in sõna]
+    kontroll = 'ABCDEFGHIJKLMNOPQRSTUVWYZÕÄÖÜ'
 
+    while elud > 0:
+        print(' '.join(lüngad))
         
-while True:
-    print('Mänguks on ülespoomine. Pead tähti pakkudes ära arvama sõna enne, kui elud otsa saavad (elusid on kokku 7). Kaotad elu iga valesti pakutud tähe eest.')
-    sleep(1)
-    mode = input('Vali mängurežiim!\n8 Tähte -> 8\n 9 Tähte -> 9\n 10 Tähte -> 10\n 11 Tähte -> 11\n Eriti rasked sõnad -> r\n QUIT -> Q\n: ')
-    if mode == 'q' or mode == 'Q':
-        print('Hüvasti!')
-        break
-    else:
-        print('_'*50)
-        print(vali_sõna(mode))
+        pakkumine = input('Sisesta täht: ').upper()
+
+        if pakkumine in äraarvatud_tähed:
+            print('Seda tähte on juba pakutud. Proovi uuesti.')
+            continue
+        äraarvatud_tähed.add(pakkumine)
+
+        if pakkumine in sõna:
+            for i, täht in enumerate(sõna):
+                if täht == pakkumine:
+                    lüngad[i] = pakkumine
+        else:
+            if pakkumine.upper() not in kontroll:
+                print(f'{pakkumine} ei ole pakutav sümbol')
+            if pakkumine.upper() in kontroll:
+                elud -= 1
+                print(f'Antud tähte selles sõnas pole, {elud} elu alles')
+
+        if all(täht != '_' for täht in lüngad):
+            print(f'{sõna}\n Arvasite sõna ära!')
+            break
+
+    if elud == 0:
+        print(f"Kahjuks kaotasid. Õige sõna oli {sõna}.")
+
+def vali_sõna():
+    while True:
+        gamemode = input('Kas soovite ära arvata tavalist sõna (1) või extra-rasket sõna (2): ')
+        if gamemode == '1':
+            tähearv = int(input('Mitmetähelist sõna soovite see kord ära arvata (sõnapikkused on 8st tähest 12ni): '))
+            sobivad_sõnad = [sõna for sõna in sõnad if len(sõna) == tähearv]
+            if not sobivad_sõnad:
+                print(f"Vabandust, pole {tähearv}-tähelisi sõnu saadaval.")
+                return None
+            else:
+                return random.choice(sobivad_sõnad)
+                
+        elif gamemode == '2':
+            return random.choice(erilised_sõnad)
+        else:
+            print('Vigane sisend. Palun sisestada 1 või 2')
+
+
+with open('sõnad.txt', encoding='UTF-8') as f: #Loen failist sõnad muutujatesse sõnad/erilised_sõnad
+    erilised_sõnad = f.readline().split(',')
+    sõnad = set()
+    for rida in f:
+        rida = rida.strip().strip("'").split("', '")
+        sõnad.update(rida)
+
+print('POOMISMÄNG. Teile antakse 6 elu, teie eesmärk on ükshaaval tähti pakkudes sõna ära arvata.')
+
+poomismäng(vali_sõna())
